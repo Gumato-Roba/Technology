1e44172906076e562eadcd6f5260dc91ab4e9e5e
Gumato-Roba <gumatorobba@gmail.com>
2022-02-12 10:02:43 +0300

    first commit

---
+++ src/main/kotlin/a.kt
@@ -0,0 +1,13 @@
+fun main()
+{var name= "Gumato Roba"
+    println(name)
+ var age= 32
+    println(age)
+ var phonenumber: String= "0758003848"
+    println(phonenumber)
+ var weight= 60
+    println(weight)
+ val kenyan= true
+    println(kenyan)
+
+}
